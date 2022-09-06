e10046ffffc2bc82a630c4df372178de995bc28f
MD AL JABIR <aljabir098@gmail.com>
2022-09-07 00:26:06 +0600

    learn apis

--- master of js/api.js
+++ master of js/api.js
@@ -4,35 +4,35 @@
  *
  *********/
 
-// const dataApiSuccess = fetch('https://jsonplaceholder.typicode.com/posts');
+const dataApiSuccess = fetch('https://jsonplaceholder.typicode.com/posts');
 
-// console.log(dataApiSuccess);
+console.log(dataApiSuccess);
 
-// dataApiSuccess.then((data) => {
-//   console.log(data);
-// });
+dataApiSuccess.then((data) => {
+  console.log(data);
+});
 
-//unscceess data of api
+unscceess data of api
 
-// const dataApiFailed = fetch(
-//   'https://jsonplaceholderdoesntexist.typicode.com/posts'
-// );
+const dataApiFailed = fetch(
+  'https://jsonplaceholderdoesntexist.typicode.com/posts'
+);
 
-// console.log(dataApiFailed);
+console.log(dataApiFailed);
 
-// dataApiFailed
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.log('Your Error:', err);
-//   });
+dataApiFailed
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log('Your Error:', err);
+  });
 
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//   .then((data) => {
-//     return data.json();
-//   })
-//   .then((posts) => {
-//     console.log(posts);
-//   });
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((data) => {
+    return data.json();
+  })
+  .then((posts) => {
+    console.log(posts);
+  });
 console.log('api.js');
