36bb4431d4ac34a555e660749c8aa42b5207180b
MD AL JABIR <aljabir098@gmail.com>
2022-09-17 13:29:19 +0600

    cb

---
+++ Again Practice/main.js
@@ -0,0 +1,14 @@
+function call_back(name, age, task) {
+  console.log('Hello ', name);
+  console.log(`I am ${age} years old`);
+}
+
+function dev() {
+  console.log(`I will be frontend developer`);
+}
+
+function target() {
+  console.log(`I want to be mern stack developer`);
+}
+
+call_back('I am Jabir', 19);
