58550c3db536fa50bb0e22f37ae3017a0f0b3d0e
al-jabir <aljabir098@gmail.com>
2022-10-27 18:33:42 +0600

    cutting string

---
+++ Test Practice/string.js
@@ -0,0 +1,10 @@
+let keteFelbo = 'Toke ami kete felbo';
+
+console.log(keteFelbo.slice(0, 4));
+console.log(keteFelbo.slice(9, 13));
+
+console.log(keteFelbo.substring(14, 19));
+
+console.log(keteFelbo.substr(9, 14));
+
+console.log(keteFelbo.length);
